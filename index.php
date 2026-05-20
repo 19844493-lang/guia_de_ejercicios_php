0ebb31ffc31b958e545024ba9e1fecd0113e8d84
Josh <19844493@clases.edu.sv>
2026-05-20 08:06:29 -0600

    Este archivo es el principal.
    Este archivo con tiene los links de todos los ejercicios.

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+echo "<body bgcolor='#6f90c2' text='#111010'>";
+echo "<h1>Complejo Educativo Alejandro de Humboldt</h1>";
+echo "<h2>Ejercicios de PHP</h2>";
+echo "<h3> Creador: Carlos Jose Medrano Lopez 1-14</h3>";
+echo "<br>";
+echo "Seleccione el archivo que queria abrir";
+echo "<br><br>";
+
+echo "<hr>";
+echo "<br>";
+
+echo "<table border='0' width='80%'>";
+echo "<tr>";
+
+echo "<td width='33%' valign='top'>";
+
+echo "1. <a href='ejercicio1.html'>Ejercicio 1: Bienvenida Basica</a>";
+echo "<br><br>";
+
+echo "2. <a href='ejercicio2.html'>Ejercicio 2: Suma de Números</a>";
+echo "<br><br>";
+
+echo "3. <a href='ejercicio3.html'>Ejercicio 3: Resta de Números</a>";
+echo "<br><br>";
+
+echo "4. <a href='ejercicio4.html'>Ejercicio 4: Multiplicación de Números</a>";
+echo "<br><br>";
+
+echo "5. <a href='ejercicio5.html'>Ejercicio 5: División de números</a>";
+echo "<br><br>";
+
+echo "6. <a href='ejercicio6.html'>Ejercicio 6: Formulario de Registro</a>";
+echo "<br><br>";
+
+echo "7. <a href='ejercicio7.html'>Ejercicio 7: Número Mayor</a>";
+echo "<br><br>";
+
+echo "8. <a href='ejercicio8.html'>Ejercicio 8: Número Menor</a>";
+echo "<br><br>";
+
+echo "9. <a href='ejercicio9.html'>Ejercicio 9: Número Par o Impar</a>";
+echo "<br><br>";
+
+echo "10. <a href='ejercicio10.html'>Ejercicio 10: Cálculo de Promedio</a>";
+echo "<br><br>";
+
+echo "</td>";
+
+echo "<td width='33%' valign='top'>";
+
+echo "11. <a href='ejercicio11.html'>Ejercicio 11: Promedio (Aprobado/Reprobado)</a>";
+echo "<br><br>";
+
+echo "12. <a href='ejercicio12.html'>Ejercicio 12: Área de Rectángulo</a>";
+echo "<br><br>";
+
+echo "13. <a href='ejercicio13.html'>Ejercicio 13: Perímetro de Cuadrado</a>";
+echo "<br><br>";
+
+echo "14. <a href='ejercicio14.html'>Ejercicio 14: Metros a Centímetros</a>";
+echo "<br><br>";
+
+echo "15. <a href='ejercicio15.html'>Ejercicio 15: Celsius a Fahrenheit</a>";
+echo "<br><br>";
+
+echo "16. <a href='ejercicio16.html'>Ejercicio 16: Salario de Empleado</a>";
+echo "<br><br>";
+
+echo "17. <a href='ejercicio17.html'>Ejercicio 17: Descuento del 10%</a>";
+echo "<br><br>";
+
+echo "18. <a href='ejercicio18.html'>Ejercicio 18: Factura de Venta</a>";
+echo "<br><br>";
+
+echo "19. <a href='ejercicio19.html'>Ejercicio 19: Tabla de Multiplicar</a>";
+echo "<br><br>";
+
+echo "20. <a href='ejercicio20.html'>Ejercicio 20: Suma Consecutiva</a>";
+echo "<br><br>";
+
+echo "</td>";
+
+echo "<td width='50%' valign='top'>";
+
+echo "21. <a href='ejercicio21.html'>Ejercicio 21: Contar Letras</a>";
+echo "<br><br>";
+
+echo "22. <a href='ejercicio22.html'>Ejercicio 22: Conversión de Dólares a Euros</a>";
+echo "<br><br>";
+
+echo "23. <a href='ejercicio23.html'>Ejercicio 23: Cálculo con IVA</a>";
+echo "<br><br>";
+
+echo "24. <a href='ejercicio24.html'>Ejercicio 24: Edad + 10 años</a>";
+echo "<br><br>";
+
+echo "25. <a href='ejercicio25.html'>Ejercicio 25: Resumen de Usuario</a>";
+echo "<br><br>";
+
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+?>
